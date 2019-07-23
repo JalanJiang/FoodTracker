68cfda6c6b5f7a86c19c7e9d585140922504fe04
JalanJiang <448300947@qq.com>
2019-07-23 22:06:52 +0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jalan_Jiang on 2019/7/23.
+//  Copyright © 2019 Jalan_Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
