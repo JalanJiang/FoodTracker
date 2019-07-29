067c894db1e6953fc182a97f0854935c280d0abe
JalanJiang <448300947@qq.com>
2019-07-29 18:24:45 +0800

    feat(*): Implement a Custom Control Part.2

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
     
     // MARK: Properties
-    private var ratingButtons = [UIButton]() // 按钮列表
-    var rating = 0 // 评级
+    // 按钮列表
+    private var ratingButtons = [UIButton]()
+    // 评级
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            // 当属性值重新被设置时创建按钮
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            // 当属性值重新被设置时创建按钮
+            setupButtons()
+        }
+    }
     
     // MARK: Initialization
     // 初始化视图
@@ -27,19 +45,39 @@ class RatingControl: UIStackView {
     }
     
     // MARK: Private Methods
+    // 创建按钮
     private func setupButtons() {
         
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightStar", in: bundle, compatibleWith: self.traitCollection)
+        
         // 循环创建 5 个按钮
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            // button.backgroundColor = UIColor.red
+            
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false // 禁用按钮自动生成的约束
             // isActive 用于启用或停用约束
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true // 高度约束
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true // 宽度约束
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true // 高度约束
+            button.widthAnchor.constraint(equalToConstant: starSize.height).isActive = true // 宽度约束
             
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -51,10 +89,37 @@ class RatingControl: UIStackView {
             ratingButtons.append(button)
         }
         
+        // 添加按钮后根据评分更新按钮状态
+        updateButtonSelectionStates()
+        
     }
     
     // MARK: Button Action
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        // print("Button pressed 👍")
+        
+        // 获取按钮下标
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
+    }
+    
+    // 更新按钮状态
+    @objc func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
     }
 }
