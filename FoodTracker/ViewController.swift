b953d1ade622ca36fe80727b6bd50d89dfb5fa61
JalanJiang <448300947@qq.com>
2019-07-30 18:39:44 +0800

    feat(*): Implement a Custom Control - Connect the Rating Control to the View Controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,9 +58,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 
     // MARK: Actions
     
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        nameTextField.text = "Default Text"
-    }
+//    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+//        nameTextField.text = "Default Text"
+//    }
     
     // 手势识别的单点操作触发
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
